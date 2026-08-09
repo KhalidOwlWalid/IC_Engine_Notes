6924ab26a34bc19078c65720d9b5bafcf00acdaa
KhalidOwlWalid <khalidowlwalid@gmail.com>
2026-08-09 13:31:25 +0100

    [refactor] Use of dataclass for EngineGeometry and Wiebe parameters into FiniteHeatRelease model

--- python/engine_lib/engine_kinematics.py
+++ python/engine_lib/engine_kinematics.py
@@ -1,49 +1,85 @@
 from dataclasses import dataclass
+from typing import Optional
 
 import numpy as np
 
+class EngineGeometryError(ValueError):
+    """Raised when EngineGeometry parameters are missing or mutually inconsistent"""
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 @dataclass(frozen=True)
 class EngineGeometry:
-    conrod_length: float
+    conrod_length: float | None # NOT USED YET 
     stroke: float
     bore: float
     compression_ratio: float
     N_c: float # Number of cylinders
+    V_bdc: float
+    V_tdc: float
+
+    @classmethod
+    def from_bore_stroke(cls, stroke: float, bore: float, compression_ratio: float, N_c: float):
+        V_d = N_c * (np.pi / 4) * np.power(bore, 2) * compression_ratio 
+        V_tdc = V_d / (compression_ratio - 1)
+        V_bdc = (V_d + V_tdc)
+        return cls(conrod_length=None, stroke=stroke, bore=bore, compression_ratio=compression_ratio, N_c=N_c, V_tdc=V_tdc, V_bdc=V_bdc)
+
+    @classmethod
+    def from_volume(cls, V_bdc: float, V_tdc: float):
+        if (V_bdc <= V_tdc):
+            raise EngineGeometryError(f"V_bdc ({V_bdc}) is less than V_tdc ({V_tdc})")
+        compression_raio = V_bdc / V_tdc
+        return cls(conrod_length=None, stroke=None, bore=None, compression_ratio=compression_raio, N_c=None, V_tdc=V_tdc, V_bdc=V_bdc)
+
+    @classmethod
+    def from_all(cls, stroke: float, bore: float, N_c: float, V_bdc: float, V_tdc: float):
+        if (V_bdc <= V_tdc):
+            raise EngineGeometryError(f"V_bdc ({V_bdc}) is less than V_tdc ({V_tdc})")
+        compression_ratio = V_bdc/V_tdc
+        return cls(conrod_length=None, stroke=stroke, bore=bore, compression_ratio=compression_ratio, N_c=N_c, V_bdc=V_bdc, V_tdc=V_tdc)
 
     @property
-    def V_d(self):
-        return self.N_c * (np.pi / 4) * np.power(self.B, 2) * self.S 
+    def V_d(self) -> float:
+        return self.V_bdc - self.V_tdc
 
     @property
-    def B(self):
+    def V_c(self) -> float:
+        return self.V_tdc
+
+    @property
+    def B(self) -> float:
+        if (self.bore is None):
+            raise EngineGeometryError("Bore, B is not provided. Use of the bore parameter will result in bug")
         return self.bore
 
     @property
-    def S(self):
+    def S(self) -> float:
+        if (self.stroke is None):
+            raise EngineGeometryError("Bore, B is not provided. Use of the bore parameter will result in bug")
         return self.stroke
 
     @property
-    def V_bdc(self):
-        pass
-
-    @property
-    def r(self):
+    def r(self) -> float:
         return self.compression_ratio
 
-    @property
-    def V_c(self):
-        return self.V_d / (self.r - 1)
+    def V_tilda(self, theta: float | np.ndarray) -> float | np.ndarray:
+        return 1/self.r + ((self.r - 1)/(2 * self.r)) * (1 - np.cos(theta))
+
+    def dVtilda_dtheta(self, theta: float | np.ndarray) -> float | np.ndarray:
+        return (self.r - 1) * np.sin(theta) / (2 * self.r)
 
     # Normalized cylinder volume at a given crank angle
     # Note that in Engineering Fundamentals of the Internal Combustion Engine by Willard W. Pulkrabek, he used a different formula
     # See equation 2-13 and 2-14
     # Note: The equation taken from 2.38 from the textbook is wrong it should have been 1/r + ... instead of 1 + ...
     @staticmethod
-    def V_tilda(r, theta):
+    def V_tilda_static(r, theta):
         return 1/r + ((r - 1)/(2 * r)) * (1 - np.cos(theta))
 
     # Normalized dv_dtheta at a given crank angle
     @staticmethod
-    def dVtilda_dtheta(r: float, theta: float | np.ndarray) -> float | np.ndarray:
+    def dVtilda_dtheta_static(r: float, theta: float | np.ndarray) -> float | np.ndarray:
         return (r - 1) * np.sin(theta) / (2 * r)
--- python/engine_lib/finite_heat_release.py
+++ python/engine_lib/finite_heat_release.py
@@ -1,7 +1,10 @@
-from dataclasses import dataclass
-from engine_lib.engine_kinematics import EngineGeometry
+from dataclasses import dataclass, field
 
 import numpy as np
+from scipy.integrate import solve_ivp
+
+from engine_lib.engine_kinematics import EngineGeometry
+
 
 @dataclass
 class WiebeParams:
@@ -13,25 +16,28 @@ class WiebeParams:
 
 @dataclass
 class FiniteHeatReleaseResults:
-    theta: np.ndarray
-    
+    theta: np.ndarray = field(default_factory=list)
+
     # Energy release results
-    xb: np.ndarray
-    dxb_dtheta: np.ndarray # [J/deg]
+    xb: np.ndarray = field(default_factory=list)
+    dxb_dtheta: np.ndarray = field(default_factory=list) # [J/deg]
 
     # Cylinder volume [m3]
-    dvtilda_dtheta: np.ndarray
-    dv_dtheta: np.ndarray
-    v: np.ndarray
-    
+    dvtilda_dtheta: np.ndarray = field(default_factory=list)
+    dv_dtheta: np.ndarray = field(default_factory=list)
+    v: np.ndarray = field(default_factory=list)
+
     # Pressure [Pa]
-    dptilda_dtheta: np.ndarray
-    dp_dtheta: np.ndarray
-    p: np.ndarray
+    dptilda_dtheta: np.ndarray = field(default_factory=list)
+    dp_dtheta: np.ndarray = field(default_factory=list)
+    p: np.ndarray = field(default_factory=list)
 
     # Work [J]
-    dqtilda_dtheta: np.ndarray
-    q: np.ndarray
+    dqtilda_dtheta: np.ndarray = field(default_factory=list)
+    q: np.ndarray = field(default_factory=list)
+
+    # Temperature
+    T: np.ndarray = field(default_factory=list)
 
 class FiniteHeatRelease:
 
@@ -43,76 +49,114 @@ class FiniteHeatRelease:
     Taken from page p.42 to p.47
     """
 
-    def __init__(self, wiebe_params: WiebeParams):
-        self.a = wiebe_params.a
-        self.n = wiebe_params.n
-        self.theta_s = wiebe_params.theta_s
-        self.theta_d = wiebe_params.theta_d
-        self.q_in = wiebe_params.q_in
-        
-        self._results = None
+    def __init__(self, eng_geometry: EngineGeometry, wiebe_params: WiebeParams):
+        self._wb_parm = wiebe_params
+        self._eng_geom = eng_geometry
 
     def wiebe_fcn(self, theta):
-        return 1 - np.exp(-self.a * np.pow((theta - self.theta_s)/self.theta_d, self.n))
+        return 1 - np.exp(-self._wb_parm.a * np.pow((theta - self._wb_parm.theta_s)/self._wb_parm.theta_d, self._wb_parm.n))
 
     def xb(self, theta):
         return self.wiebe_fcn(theta)
 
     # Equation 2.26, can also be re-written as Equation 2.25
     def dxb_dtheta(self, theta: float | np.ndarray) -> float | np.ndarray:
-        X = (theta - self.theta_s) / self.theta_d
-        dx_dtheta = (self.a * self.n / self.theta_d) * np.power(X, self.n - 1) * np.exp(-self.a * np.power(X, self.n))
+        X = (theta - self._wb_parm.theta_s) / self._wb_parm.theta_d
+        dx_dtheta = (self._wb_parm.a * self._wb_parm.n / self._wb_parm.theta_d) * np.power(X, self._wb_parm.n - 1) * np.exp(-self._wb_parm.a * np.power(X, self._wb_parm.n))
         return dx_dtheta
 
-    # TODO: Refactor this, this is a lot of just hardcoded implementation
-    # TODO: Provide an option to solve with ODE solver (e.g. RK45) instead of forward-euler
-    def solve_dptilda_dtheta(self, r: float | np.ndarray, theta: float | np.ndarray, V_bdc: float, gamma: float, P_0: float) -> FiniteHeatReleaseResults:
+    def _dp_dtheta_ode(self, theta, y, gamma):
+
+        P = y[0]
+
+        # Calculate the cylinder volume
+        Vtilda = EngineGeometry.V_tilda(self._eng_geom.r, theta)
+        V = Vtilda * self._eng_geom.V_bdc
+
+        dVtilda_dtheta = EngineGeometry.dVtilda_dtheta(self._eng_geom.r, theta)
+        dV_dtheta = dVtilda_dtheta * self._eng_geom.V_bdc
+
+        if (theta < self._wb_parm.theta_s or theta > (self._wb_parm.theta_s + self._wb_parm.theta_d)):
+            Q_in = 0
+            dx_dtheta = 0
+        else:
+            Q_in = self._wb_parm.q_in
+            dx_dtheta = self.dxb_dtheta(theta)
+
+        dp_dtheta = -gamma * (P / V) * dV_dtheta + (gamma - 1) * (Q_in / V) * dx_dtheta
+
+        return [dp_dtheta]
+
+    def solve_pressure_ivp(self, theta, gamma, iv_P):
+        """Solves the pressure equation (dP/dtheta) via initial value solver
+
+        Args:
+            theta (_type_): _description_
+            gamma (_type_): _description_
+            iv_P (_type_): _description_
+
+        Raises:
+            RuntimeError: _description_
+
+        Returns:
+            _type_: _description_
+        """
+        sol = solve_ivp(
+            fun=self._dp_dtheta_ode,
+            t_span=(theta[0], theta[-1]),
+            y0=[iv_P],
+            t_eval=theta,
+            args=[gamma]
+        )
+
+        if not sol.success:
+            raise RuntimeError(f"Solve IVP failed: {sol.message}")
+        return sol
+
+    # Forward-Euler solver, leaving this here but user should be using the solve_pressure_ivp function instead
+    def solve(self, r: float | np.ndarray, theta: float | np.ndarray, V_bdc: float, gamma: float, P_0: float) -> FiniteHeatReleaseResults:
         """
         Solve the dP/dtheta problem using forward-euler, to yield higher accuracy, consider using RK45 (Runge Kutta method).
 
         Initial pressure, P_0 required to solve the equation as P in itself is a state with memory
         """
 
-        results = FiniteHeatReleaseResults
-
-        P_results = []
-        burn_rate = []
-        T_results = []
+        results = FiniteHeatReleaseResults()
 
         # Initial conditions
-        P_0 = 1e5
         P = P_0 # Pa
-        P_results.append(P / 1000)
         Q_in = 0
-        T_0 = 300 # K
-        T_results.append(T_0)
+
+        results.p.append(P)
         for i in range(len(theta)):
             
             # Calculate the burn rate
             dx_dtheta = self.dxb_dtheta(theta[i])
-            burn_rate.append(dx_dtheta)
+            results.dxb_dtheta.append(dx_dtheta)
 
             # Calculate the cylinder volume
             Vtilda = EngineGeometry.V_tilda(r, theta[i])
             V = Vtilda * V_bdc
+            results.v.append(V)
 
             dVtilda_dtheta = EngineGeometry.dVtilda_dtheta(r, theta[i])
             dV_dtheta = dVtilda_dtheta * V_bdc
+            results.dv_dtheta.append(dV_dtheta)
 
             if (theta[i] < self.theta_s or theta[i] > (self.theta_s + self.theta_d)):
                 Q_in = 0
             else:
                 Q_in = self.q_in
+            results.q.append(Q_in)
 
             dP_dtheta = -gamma * (P / V) * dV_dtheta + (gamma - 1) * (Q_in / V) * dx_dtheta
+            results.dp_dtheta.append(dP_dtheta)
 
             if (i < len(theta) - 1):
                 P = P + dP_dtheta * (theta[i + 1] - theta[i])
-                curr_T = T_0 * (P / P_0) * (V / V_bdc)
-                T_results.append(curr_T)
-                P_results.append(P / 1000) # Converted into kPa
+                results.p.append(P)
 
-        return P_results, burn_rate, T_results
+        return results 
 
 class FiniteHeatReleaseWithLoss:
     pass
