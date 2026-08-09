[bugfix] Fix V_d calculation
- Should have been multiplied by stroke instead of compression_ratio

python/engine_lib/engine_kinematics.py
@@ -22,7 +22,7 @@ class EngineGeometry:
 
     @classmethod
     def from_bore_stroke(cls, stroke: float, bore: float, compression_ratio: float, N_c: float):
-        V_d = N_c * (np.pi / 4) * np.power(bore, 2) * compression_ratio 
+        V_d = N_c * (np.pi / 4) * np.power(bore, 2) * stroke 
         V_tdc = V_d / (compression_ratio - 1)
         V_bdc = (V_d + V_tdc)
         return cls(conrod_length=None, stroke=stroke, bore=bore, compression_ratio=compression_ratio, N_c=N_c, V_tdc=V_tdc, V_bdc=V_bdc)

python/engine_lib/finite_heat_release.py
@@ -50,8 +50,8 @@ class FiniteHeatRelease:
     """
 
     def __init__(self, eng_geometry: EngineGeometry, wiebe_params: WiebeParams):
-        self._wb_parm = wiebe_params
         self._eng_geom = eng_geometry
+        self._wb_parm = wiebe_params
 
     def wiebe_fcn(self, theta):
         return 1 - np.exp(-self._wb_parm.a * np.pow((theta - self._wb_parm.theta_s)/self._wb_parm.theta_d, self._wb_parm.n))
@@ -70,10 +70,10 @@ class FiniteHeatRelease:
         P = y[0]
 
         # Calculate the cylinder volume
-        Vtilda = EngineGeometry.V_tilda(self._eng_geom.r, theta)
+        Vtilda = self._eng_geom.V_tilda(theta)
         V = Vtilda * self._eng_geom.V_bdc
 
-        dVtilda_dtheta = EngineGeometry.dVtilda_dtheta(self._eng_geom.r, theta)
+        dVtilda_dtheta = self._eng_geom.dVtilda_dtheta(theta)
         dV_dtheta = dVtilda_dtheta * self._eng_geom.V_bdc
 
         if (theta < self._wb_parm.theta_s or theta > (self._wb_parm.theta_s + self._wb_parm.theta_d)):
@@ -135,18 +135,18 @@ class FiniteHeatRelease:
             results.dxb_dtheta.append(dx_dtheta)
 
             # Calculate the cylinder volume
-            Vtilda = EngineGeometry.V_tilda(r, theta[i])
+            Vtilda = EngineGeometry.V_tilda_static(r, theta[i])
             V = Vtilda * V_bdc
             results.v.append(V)
 
-            dVtilda_dtheta = EngineGeometry.dVtilda_dtheta(r, theta[i])
+            dVtilda_dtheta = EngineGeometry.dVtilda_dtheta_static(r, theta[i])
             dV_dtheta = dVtilda_dtheta * V_bdc
             results.dv_dtheta.append(dV_dtheta)
 
-            if (theta[i] < self.theta_s or theta[i] > (self.theta_s + self.theta_d)):
+            if (theta[i] < self._wb_parm.theta_s or theta[i] > (self._wb_parm.theta_s + self._wb_parm.theta_d)):
                 Q_in = 0
             else:
-                Q_in = self.q_in
+                Q_in = self._wb_parm.q_in
             results.q.append(Q_in)
 
             dP_dtheta = -gamma * (P / V) * dV_dtheta + (gamma - 1) * (Q_in / V) * dx_dtheta

python/exercise/chapter_2/example-2_2.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from engine_lib.finite_heat_release import FiniteHeatRelease, WiebeParams
+from engine_lib.engine_kinematics import EngineGeometry
 
 """
 Example 2.2
@@ -23,30 +24,32 @@ and imep of the engine.
 """
 def main():
 
-    theta = np.linspace(-np.pi, np.pi, 100)
+    theta = np.linspace(-np.pi, np.pi, 200)
     theta_deg = theta * 180 / np.pi
 
-    engine1_wb_params = WiebeParams(
-        a=5, n=3, theta_s=np.deg2rad(0), theta_d=np.deg2rad(40), q_in=1764
-    )
+    engine_geometry = EngineGeometry.from_bore_stroke(bore=100e-3, stroke=100e-3, compression_ratio=10, N_c=1)
+    engine2_geometry = EngineGeometry.from_bore_stroke(bore=100e-3, stroke=100e-3, compression_ratio=10, N_c=1)
 
-    engine2_wb_params = WiebeParams(
+    engine1_wb_params = WiebeParams(
         a=5, n=3, theta_s=np.deg2rad(-20), theta_d=np.deg2rad(40), q_in=1764
     )
 
-    engine1 = FiniteHeatRelease(engine1_wb_params)
-    engine2 = FiniteHeatRelease(engine2_wb_params)
-    engine1_P, engine1_burn_rate, engine1_temp = engine1.solve_dptilda_dtheta(10, theta, 8.73e-4, 1.4, 100e3)
-    engine2_P, engine2_burn_rate, engine2_temp = engine2.solve_dptilda_dtheta(10, theta, 8.73e-4, 1.4, 100e3)
+    engine2_wb_params = WiebeParams(
+        a=5, n=3, theta_s=np.deg2rad(0), theta_d=np.deg2rad(40), q_in=1764
+    )
 
-    plt.plot(theta_deg, engine1_temp, label="Engine 1")
-    plt.plot(theta_deg, engine2_temp, label="Engine 2")
-    # plt.plot(theta_deg, engine1_P, label="Engine 1")
-    # plt.plot(theta_deg, engine2_P, label="Engine 2")
-    plt.title("Different crank angle start comparison")
-    plt.ylabel("Pressure (kPa)")
-    plt.xlabel("Theta (degree)")
-    plt.grid()
+    engine1 = FiniteHeatRelease(engine_geometry, engine1_wb_params)
+    engine2 = FiniteHeatRelease(engine2_geometry, engine2_wb_params)
+
+    engine1_sol = engine1.solve_pressure_ivp(theta=theta, gamma=1.4, iv_P=100e3)
+    # engine1_sol_fe = engine1.solve(r=10, theta=theta, V_bdc=8.73e-4, gamma=1.4, P_0=100e3)
+    engine2_sol = engine2.solve_pressure_ivp(theta=theta, gamma=1.4, iv_P=100e3)
+
+    theta_deg = theta * 180/np.pi
+
+    plt.plot(theta_deg, engine1_sol.y[0], label="Engine 1 (RK45)")
+    # plt.plot(theta_deg, engine1_sol_fe.p, label="Engine 1 (Forwar-Euler)")
+    plt.plot(theta_deg, engine2_sol.y[0], label="Engine 2 (RK45)")
     plt.legend()
     plt.show()
 